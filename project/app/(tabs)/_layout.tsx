cd9433ef1851f5f0ec37eb7d6bd3a405b9dbadca
Ayoub Loueti <ayoub.loueti1@gmail.com>
2025-06-12 10:19:48 +0100

    front

--- project/app/(tabs)/_layout.tsx
+++ project/app/(tabs)/_layout.tsx
@@ -68,7 +68,7 @@ export default function TabLayout() {
           ),
         }}
       />
-     <Tabs.Screen
+      <Tabs.Screen
         name="edit-profile"
         options={{
           href: null, // Hide from tab bar
